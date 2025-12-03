981b58aff379e803beec842e781defeab9a9e122
rezaarbab <92266469+rezaarbab@users.noreply.github.com>
2025-12-03 07:13:25 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:begzar/widgets/navigation_rail_widget.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_v2ray/model/v2ray_status.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:safe_device/safe_device.dart';
 
